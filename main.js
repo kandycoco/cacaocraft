a456419c5bdc40c45a5c58c3baa36d05d94c899c
kandycoco <34054580+kandycoco@users.noreply.github.com>
2017-11-27 23:18:07 -0800

    Add files via upload
    initial set up + mines working

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var mineField = [];
+
+function loadMines(){
+    for(var i=0; i<4; i++){
+        var mineRow = [];
+        for(var j=0; j<5; j++){
+            if(Math.random()<.2){
+                mineRow.push(true);
+            }else{
+                mineRow.push(false);   
+            }
+        }
+        mineField.push(mineRow);
+    }
+}
+
+loadMines();
+
+function changeBlock(x,y){
+    var id = 'block_' + x + y;
+    var element = document.getElementById(id);
+    if(mineField[y][x]){
+        element.setAttribute('class', 'block mines');
+        alert("LEL BARDTOX");
+        window.location.reload();
+    }else{
+        element.setAttribute('class', 'block clicked');
+    }
+}
